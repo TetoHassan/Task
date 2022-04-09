ca05ab32c741a3323ef09b210db78d11baa9973a
Taher Hassan <you@example.com>
2022-04-10 00:50:56 +0200

    Task

---
+++ main.js
@@ -0,0 +1,31 @@
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+
+window.onscroll = function () {
+  if (window.pageYOffset >= menu.offsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+// carousel
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
